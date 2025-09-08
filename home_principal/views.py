50b7ba33bbe9a016c484a1b19c0e14241cb0f969
RegisChip <l23120492@morelia.tecnm.mx>
2025-09-08 10:05:40 -0600

    Modificacion menor

--- home_principal/views.py
+++ home_principal/views.py
@@ -8,7 +8,12 @@ def principal(request):
     # Render de paginaPrin.html de templates
 
 def database(request):
-    user = Usuario.objects.all()
+    try:
+        user = Usuario.objects.all()
+        db_ok = True
+    except Exception:
+        user = []
+        db_ok 
     return render(request, 'bd_exitosa.html',
-                  {'user': user})
+                  {'db_ok': db_ok ,'user': user})
     # Render de bd_exitosa.html de templates
