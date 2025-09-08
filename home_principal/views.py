9b5f46c041a6609641610919f6135d08ea1c6cad
RegisChip <l23120492@morelia.tecnm.mx>
2025-09-07 18:46:18 -0600

    PaginaPrincipal agregada

--- home_principal/views.py
+++ home_principal/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def principal(request):
+    return render(request, 'paginaPrin.html') 
+    # Render de paginaPrin.html de templates
